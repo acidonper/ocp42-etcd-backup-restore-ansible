1c336c44fb5cf8b46685f8fcb48bc26a8ff0de70
Asier Cidon <acidonpe@redhat.com>
2020-01-20 14:31:11 +0100

    Added a filter in order to find patterns in a string array and ssh proxy command configuration

---
+++ plugins/filter/list_patterns.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2017 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.0',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+
+from ansible.errors import AnsibleFilterError
+from ansible.module_utils.six.moves.urllib.parse import urlsplit
+from ansible.utils import helpers
+
+
+def list_patterns(value, query):
+
+    if query == '':
+        raise AnsibleFilterError('No query included')
+    else:
+        if isinstance(value, list):
+            matching = [s for s in value if query in s]
+            return matching
+        else:
+            raise AnsibleFilterError('Object is not a List')
+
+
+# ---- Ansible filters ----
+class FilterModule(object):
+    ''' Find string patterns in a string list'''
+
+    def filters(self):
+        return {
+            'list_patterns': list_patterns
+        }
